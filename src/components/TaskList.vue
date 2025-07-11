c87df366c1ee7f8fea71e2c2e7ba551bc5ecc480
Edu <eduardo.caro.lorente@gmail.com>
2025-07-11 16:44:43 +0200

    Añadido componente TaskItem y modificado componente TaskList

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,10 +1,49 @@
 <template>
-    <div></div>
+    <transition-group name="task" tag="ul" class="space-y-2 relative">
+        <TaskItem v-for="task in store.tasks" :key="task.id" :task="task" @remove="store.removeTask(task.id)"/>
+    </transition-group>
 </template>
 
 <script setup>
+import {useTodoStore} from '../stores/todoStore'
+import TaskItem from './TaskItem.vue'
 
+const store = useTodoStore()
 </script>
 
 <style scoped>
+.task-enter-from{
+    opacity: 0;
+    transform: scale(0.7);
+}
+
+.task-enter-active{
+    transition: transform 600ms cubic-bezier(0.68, -0.55, 0.27, 1.55), opacity 300ms ease-out;
+}
+
+.task-enter-to{
+    opacity: 1;
+    transform: scale(1);
+}
+
+.task-leave-from{
+    opacity: 1;
+    transform: translateX(0px);
+}
+
+.task-leave-active{
+    position: absolute;
+    width: 100%;
+    min-height: 40px;
+    transition: transform 300ms ease-in, opacity 300ms ease-in;
+}
+
+.task-leave-to{
+    opacity: 0;
+    transform: translateX(100px)
+}
+
+.task-move{
+    transition: transform 300ms ease;
+}
 </style>
